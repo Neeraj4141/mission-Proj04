51837dbce352ed7557c10ef02903da64f735429e
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-01-15 18:02:46 +0530

    Update JDBCDataSource.java

--- Project-4/ORSProject-04/src/main/java/in/co/rays/proj4/util/JDBCDataSource.java
+++ Project-4/ORSProject-04/src/main/java/in/co/rays/proj4/util/JDBCDataSource.java
@@ -5,68 +5,74 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ResourceBundle;
-
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public final class JDBCDataSource {
 
-	private static JDBCDataSource jds = null;
+    private static JDBCDataSource jds = null;
+    private ComboPooledDataSource cpds = null;
 
-	private ComboPooledDataSource cpds = null;
+    private static ResourceBundle rb =
+            ResourceBundle.getBundle("in.co.rays.proj4.bundle.system");
 
-	private static ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.proj4.bundle.system");
+    private JDBCDataSource() {
+        try {
+            cpds = new ComboPooledDataSource();
+            cpds.setDriverClass(rb.getString("driver"));
 
-	private JDBCDataSource() {
-		try {
-			cpds = new ComboPooledDataSource();
-			cpds.setDriverClass(rb.getString("driver"));
-			cpds.setJdbcUrl(rb.getString("url"));
-			cpds.setUser(rb.getString("username"));
-			cpds.setPassword(rb.getString("password"));
-			cpds.setInitialPoolSize(Integer.parseInt(rb.getString("initialpoolsize")));
-			cpds.setAcquireIncrement(Integer.parseInt(rb.getString("acquireincrement")));
-			cpds.setMaxPoolSize(Integer.parseInt(rb.getString("maxpoolsize")));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+            /* 🔥 AUTO DETECT ENVIRONMENT */
+            String hostname = System.getenv("HOSTNAME");   // Docker/Jenkins me hota hai
 
-	public static JDBCDataSource getInstance() {
-		if (jds == null) {
-			jds = new JDBCDataSource();
-		}
-		return jds;
-	}
+            if (hostname != null && hostname.contains("ORS")) {
+                // Docker / Jenkins
+                cpds.setJdbcUrl(rb.getString("url.docker"));
+            } else {
+                // Local Laptop
+                cpds.setJdbcUrl(rb.getString("url.local"));
+            }
 
-	public static Connection getConnection() {
-		try {
-			return getInstance().cpds.getConnection();
-		} catch (SQLException e) {
-			return null;
-		}
-	}
+            cpds.setUser(rb.getString("username"));
+            cpds.setPassword(rb.getString("password"));
+            cpds.setInitialPoolSize(Integer.parseInt(rb.getString("initialpoolsize")));
+            cpds.setAcquireIncrement(Integer.parseInt(rb.getString("acquireincrement")));
+            cpds.setMaxPoolSize(Integer.parseInt(rb.getString("maxpoolsize")));
 
-	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
-		try {
-			if (rs != null) {
-				rs.close();
-			}
-			if (stmt != null) {
-				stmt.close();
-			}
-			if (conn != null) {
-				conn.close();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	public static void closeConnection(Connection conn, Statement stmt) {
-		closeConnection(conn, stmt, null);
-	}
+    public static JDBCDataSource getInstance() {
+        if (jds == null) {
+            jds = new JDBCDataSource();
+        }
+        return jds;
+    }
 
-	public static void closeConnection(Connection conn) {
-		closeConnection(conn, null);
-	}
+    public static Connection getConnection() {
+        try {
+            return getInstance().cpds.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) rs.close();
+            if (stmt != null) stmt.close();
+            if (conn != null) conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection conn, Statement stmt) {
+        closeConnection(conn, stmt, null);
+    }
+
+    public static void closeConnection(Connection conn) {
+        closeConnection(conn, null, null);
+    }
 }
