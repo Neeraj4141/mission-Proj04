ac9fa3fd31e51a4a8f0ac8b88e57716e52b44653
Neeraj4141 <mewadaneeraj368@gmail.com>
2026-01-16 13:44:43 +0530

    Refactor JDBCDataSource for connection management

--- Project-4/ORSProject-04/src/main/java/in/co/rays/proj4/util/JDBCDataSource.java
+++ Project-4/ORSProject-04/src/main/java/in/co/rays/proj4/util/JDBCDataSource.java
@@ -10,67 +10,63 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public final class JDBCDataSource {
 
-    private static JDBCDataSource jds = null;
-    private ComboPooledDataSource cpds = null;
+	private static JDBCDataSource jds = null;
 
-    private static ResourceBundle rb =
-            ResourceBundle.getBundle("in.co.rays.proj4.bundle.system");
+	private ComboPooledDataSource cpds = null;
 
-    private JDBCDataSource() {
-        try {
-            cpds = new ComboPooledDataSource();
-            cpds.setDriverClass(rb.getString("driver"));
+	private static ResourceBundle rb = ResourceBundle.getBundle("in.co.rays.proj4.bundle.system");
 
-            // 🔥 FINAL ENV SWITCH (Industry Standard)
-            String env = System.getProperty("env");
+	private JDBCDataSource() {
+		try {
+			cpds = new ComboPooledDataSource();
+			cpds.setDriverClass(rb.getString("driver"));
+			cpds.setJdbcUrl(rb.getString("url"));
+			cpds.setUser(rb.getString("username"));
+			cpds.setPassword(rb.getString("password"));
+			cpds.setInitialPoolSize(Integer.parseInt(rb.getString("initialpoolsize")));
+			cpds.setAcquireIncrement(Integer.parseInt(rb.getString("acquireincrement")));
+			cpds.setMaxPoolSize(Integer.parseInt(rb.getString("maxpoolsize")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
-            if ("docker".equalsIgnoreCase(env)) {
-                cpds.setJdbcUrl(rb.getString("url.docker"));   // Docker DB
-            } else {
-                cpds.setJdbcUrl(rb.getString("url.local"));    // Local / Jenkins Tomcat
-            }
+	public static JDBCDataSource getInstance() {
+		if (jds == null) {
+			jds = new JDBCDataSource();
+		}
+		return jds;
+	}
 
-            cpds.setUser(rb.getString("username"));
-            cpds.setPassword(rb.getString("password"));
-            cpds.setInitialPoolSize(Integer.parseInt(rb.getString("initialpoolsize")));
-            cpds.setAcquireIncrement(Integer.parseInt(rb.getString("acquireincrement")));
-            cpds.setMaxPoolSize(Integer.parseInt(rb.getString("maxpoolsize")));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+	public static Connection getConnection() {
+		try {
+			return getInstance().cpds.getConnection();
+		} catch (SQLException e) {
+			return null;
+		}
+	}
 
-    public static JDBCDataSource getInstance() {
-        if (jds == null) {
-            jds = new JDBCDataSource();
-        }
-        return jds;
-    }
+	public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    public static Connection getConnection() {
-        try {
-            return getInstance().cpds.getConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public static void closeConnection(Connection conn, Statement stmt) {
+		closeConnection(conn, stmt, null);
+	}
 
-    public static void closeConnection(Connection conn, Statement stmt, ResultSet rs) {
-        try {
-            if (rs != null) rs.close();
-            if (stmt != null) stmt.close();
-            if (conn != null) conn.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void closeConnection(Connection conn, Statement stmt) {
-        closeConnection(conn, stmt, null);
-    }
-
-    public static void closeConnection(Connection conn) {
-        closeConnection(conn, null, null);
-    }
+	public static void closeConnection(Connection conn) {
+		closeConnection(conn, null);
+	}
 }
